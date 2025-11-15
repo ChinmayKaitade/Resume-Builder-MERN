35ef1c913f1153d8f3d67073823d21ff0259cb5d
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-11-15 23:21:01 +0530

    CallToAction component code added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Banner from "../components/Home/Banner";
 import Hero from "../components/Home/Hero";
 import Features from "../components/Home/Features";
 import Testimonials from "../components/Home/Testimonials";
+import Footer from "../components/Home/Footer";
+import CallToAction from "../components/Home/CallToAction";
 
 const Home = () => {
   return (
@@ -11,6 +13,7 @@ const Home = () => {
       <Hero />
       <Features />
       <Testimonials />
+      <CallToAction />
     </div>
   );
 };
