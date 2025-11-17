a080ea481c1b24b596b75db3ca147a1ebdfe2723
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-11-17 20:06:56 +0530

    userRoute code updated

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,11 +5,13 @@ import {
   registeredUser,
 } from "../controllers/userController.js";
 import protect from "../middlewares/authMiddleware.js";
+import Resume from "../models/Resume.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registeredUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserById);
+userRouter.get("/resumes", protect, Resume);
 
 export default userRouter;
