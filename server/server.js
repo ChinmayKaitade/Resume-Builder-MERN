2dcc6517ea9c510515f126a447b254d1c4c8b539
ChinmayKaitade <chinmaykaitade123@gmail.com>
2025-11-18 07:43:12 +0530

    server code updated

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import "dotenv/config";
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
 import resumeRouter from "./routes/resumeRoutes.js";
+import aiRouter from "./routes/aiRoutes.js";
 
 const app = express();
 // PORT is read dynamically from process.env, defaulting to 3000
@@ -27,6 +28,7 @@ app.get("/", (req, res) => {
 });
 app.use("/api/users", userRouter);
 app.use("/api/resumes", resumeRouter);
+app.use("/api/ai", aiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
